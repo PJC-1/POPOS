8c84dc2ccde2e0823825b70d4b9775c474ed56c3
PJC-1 <jumpzine@gmail.com>
2018-02-07 20:43:48 -0800

    passing down some props to SpaceListItem

--- src/components/space_list.js
+++ src/components/space_list.js
@@ -4,9 +4,18 @@ import SpaceListItem from './space_list_item';
 const SpaceList = props => {
   console.log(props.spaces);
 
+  const spaceItems = props.spaces.map(space => {
+    return (
+      <SpaceListItem
+        key={space.name}
+        space={space}
+      />
+    );
+  });
+
   return (
     <div>
-      <SpaceListItem />
+      {spaceItems}
     </div>
   );
 }
