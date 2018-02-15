dccbd969fcf88400b03079cf3bc66043fa5417c1
PJC-1 <jumpzine@gmail.com>
2018-02-14 21:57:29 -0800

    building out the space show

--- src/components/space_show_detail.js
+++ src/components/space_show_detail.js
@@ -4,7 +4,19 @@ class SpaceShowDetail extends Component {
   render() {
     return (
       <div>
-        {this.props.space[0].name}
+        <header>
+          <h1 className="header-title">{this.props.space[0].name}</h1>
+          <p>{this.props.space[0].popos_addr}</p>
+        </header>
+        <div className="space-container">          
+          <div className="space-content">
+            <div className="space-content-sub">
+              <div className="space-description">
+                <p className="description">{this.props.space[0].descriptio}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
