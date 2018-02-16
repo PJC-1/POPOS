2232c7cbdec5a66c558b97065415e453daff9f72
PJC-1 <jumpzine@gmail.com>
2018-02-15 22:31:50 -0800

    styling to the space show

--- src/components/space_show_detail.js
+++ src/components/space_show_detail.js
@@ -3,12 +3,28 @@ import React, { Component } from 'react';
 class SpaceShowDetail extends Component {
   render() {
     return (
-      <div>
+      <div className="space-wrapper">
         <header>
           <h1 className="header-title">{this.props.space[0].name}</h1>
           <p>{this.props.space[0].popos_addr}</p>
         </header>
-        <div className="space-container">          
+        <div className="space-container">
+          <div className="space-content-side">
+            <div>
+              <div className="space-side-div-two">
+                <div className="space-detail-one">
+                  {this.props.space[0].hours ? (
+                    <p>{this.props.space[0].hours}</p>
+                  ) : (
+                    <p>{this.props.space[0].hours_type}</p>
+                  )}
+                </div>
+                <div className="space-detail-two">
+                  {this.props.space[0].type}
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="space-content">
             <div className="space-content-sub">
               <div className="space-description">
