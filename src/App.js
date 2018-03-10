1323eca1689a6cbe7fd485f5cb0c6caf85296178
PJC-1 <jumpzine@gmail.com>
2018-03-09 23:19:08 -0800

    added landing page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount(){
+    setTimeout(function() {
+      document.getElementById('main').classList.remove('is-loading');
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Link to="/spaces">
-          All Spaces
-        </Link>
-        <div>
-          Hello World
+        <div id="main" className="is-loading">
+        	<h1>
+        		<Link to="/spaces">P.O.P.O.S.</Link>
+        	</h1>
         </div>
       </div>
     );
